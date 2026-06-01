c27d97331d7420900abca792c7eb58b42e4cf8d6
Hacker <minubgp@gmail.com>
2026-06-01 20:55:27 +0530

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Proton
+{
+    class ProtLib
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please wait.......");
+            Console.WriteLine("Build for Proton completed for Windows");
+            Console.WriteLine("This is a test file to see if the IDE is working properly");  
+        }
+    }
+}
