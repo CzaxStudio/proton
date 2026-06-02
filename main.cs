3aa6824fd4c172d60812c772158995ab4e862205
Hacker <minubgp@gmail.com>
2026-06-02 11:23:35 +0530

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,14 +1,19 @@
 using System;
-
-namespace Proton
+using System.Diagnostics;
+// This is a test file
+namespace proton
 {
-    class ProtLib
+    class protlib
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Please wait.......");
-            Console.WriteLine("Build for Proton completed for Windows");
-            Console.WriteLine("This is a test file to see if the IDE is working properly");  
+            Console.WriteLine("please wait.......");
+            Console.WriteLine("build for proton completed for windows");
+            Console.WriteLine("this is a test file to see if the ide is working properly");
+
+            
+            Process.Start(@"C:\Users\DELL\AppData\Local\Programs\Microsoft VS Code\Code.exe");
+            // This file is not related to the Proton framework
         }
     }
 }
